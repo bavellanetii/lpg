034079539f304d211d8f4e4f0eaed055a09d984c
Brandon Avellanet <bavellanetii@gmail.com>
2023-09-13 18:12:21 +0100

    Added basic UI including progress bars

---
+++ src/app/progressbars/progressbars.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+import { NgbProgressbarModule } from '@ng-bootstrap/ng-bootstrap';
+
+@Component({
+  selector: 'app-progressbars',
+  templateUrl: './progressbars.component.html',
+  styleUrls: ['./progressbars.component.css']
+})
+export class ProgressbarsComponent {
+
+}
