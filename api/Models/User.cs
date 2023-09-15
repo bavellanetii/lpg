99f6e8281d6eb7b415c7d3143cd5ccbe496137d3
Brandon Avellanet <bavellanetii@gmail.com>
2023-09-15 18:50:31 +0100

    Added ef and data context file

---
+++ api/Models/User.cs
@@ -0,0 +1,21 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace api.Models
+{
+    public class User
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int ProgrammingLevel { get; set; }
+        public int ProgrammingXP { get; set; }
+        public int FitnessLevel { get; set; }
+        public int FitnessXP { get; set; }
+        public int HouseworkLevel { get; set; }
+        public int HouseworkXP { get; set; }
+        public int ReadingLevel { get; set; }
+        public int ReadingXP { get; set; }
+}
+}
