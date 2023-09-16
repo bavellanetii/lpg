f4174b818919eb1cefd8e96f3f9a132503d6d429
Brandon Avellanet <bavellanetii@gmail.com>
2023-09-16 11:14:26 +0100

    Implemented repository pattern

---
+++ api/Repositories/UserRepository.cs
@@ -0,0 +1,17 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using api.Data;
+
+namespace api.Repositories
+{
+        
+    public class UserRepository()
+    {
+        public UserRepository(DataContext context)
+        {
+            
+        }    
+    }
+}
