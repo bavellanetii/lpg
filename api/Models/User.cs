82da402de27b2c0ab9bb394ab5f08c9a452ef754
Brandon Avellanet <bavellanetii@gmail.com>
2023-09-30 17:01:18 +0100

    added level xp properties to user model

--- api/Models/User.cs
+++ api/Models/User.cs
@@ -10,12 +10,16 @@ namespace api.Models
         public int Id { get; set; }
         public string Name { get; set; }
         public int ProgrammingLevel { get; set; }
+        public int ProgrammingLevelXP { get; set; }
         public int ProgrammingXP { get; set; }
         public int FitnessLevel { get; set; }
+        public int FitnessLevelXP { get; set; } 
         public int FitnessXP { get; set; }
         public int HouseworkLevel { get; set; }
+        public int HouseworkLevelXP { get; set; }
         public int HouseworkXP { get; set; }
         public int ReadingLevel { get; set; }
+        public int ReadingLevelXP { get; set; }
         public int ReadingXP { get; set; }
 }
 }
