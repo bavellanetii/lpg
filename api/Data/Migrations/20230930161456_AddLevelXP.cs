db2956964735014e1188d77605166455534b3016
Brandon Avellanet <bavellanetii@gmail.com>
2023-09-30 17:17:38 +0100

    migrated and added new fields to database

---
+++ api/Data/Migrations/20230930161456_AddLevelXP.cs
@@ -0,0 +1,80 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace api.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddLevelXP : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Name",
+                table: "Users",
+                type: "nvarchar(max)",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)");
+
+            migrationBuilder.AddColumn<int>(
+                name: "FitnessLevelXP",
+                table: "Users",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.AddColumn<int>(
+                name: "HouseworkLevelXP",
+                table: "Users",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.AddColumn<int>(
+                name: "ProgrammingLevelXP",
+                table: "Users",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.AddColumn<int>(
+                name: "ReadingLevelXP",
+                table: "Users",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "FitnessLevelXP",
+                table: "Users");
+
+            migrationBuilder.DropColumn(
+                name: "HouseworkLevelXP",
+                table: "Users");
+
+            migrationBuilder.DropColumn(
+                name: "ProgrammingLevelXP",
+                table: "Users");
+
+            migrationBuilder.DropColumn(
+                name: "ReadingLevelXP",
+                table: "Users");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Name",
+                table: "Users",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)",
+                oldNullable: true);
+        }
+    }
+}
