ac2f7f947babba9148de3b987929a9d89d2a139d
Brandon Avellanet <bavellanetii@gmail.com>
2023-10-04 13:42:41 +0100

    created get method in front end

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,10 +1,11 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { ProgressbarsComponent } from './progressbars/progressbars.component';
+import { FormsModule } from '@angular/forms';
+import { HttpClientModule} from '@angular/common/http';
 
 @NgModule({
   declarations: [
@@ -14,7 +15,9 @@ import { ProgressbarsComponent } from './progressbars/progressbars.component';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    NgbModule
+    NgbModule,
+    FormsModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/progressbars/progressbars.component.ts
+++ src/app/progressbars/progressbars.component.ts
@@ -1,3 +1,4 @@
+import { HttpClient } from '@angular/common/http';
 import { Component } from '@angular/core';
 import { NgbProgressbarModule } from '@ng-bootstrap/ng-bootstrap';
 
@@ -7,5 +8,17 @@ import { NgbProgressbarModule } from '@ng-bootstrap/ng-bootstrap';
   styleUrls: ['./progressbars.component.css']
 })
 export class ProgressbarsComponent {
+  
+  constructor(private http: HttpClient){
+
+  }
+
+  private fetchUser(){
+    this.http.get('http://localhost:5100/api/User/1')
+    .subscribe((response) => {
+      console.log(response)
+    })
+  }
 
 }
+
